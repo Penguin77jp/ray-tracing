1c6d167404acbf29d33afc874d0dce29f43473c3
Penguin Koutei <Koutei7penguin.jp@gmail.com>
2019-02-23 17:04:33 +0900

    up

--- ray-tracing/render.cpp
+++ ray-tracing/render.cpp
@@ -33,16 +33,14 @@ void render(Screen &getScreen) {
 			//std::cout << "done" << std::endl;
 		//}
 		auto info = RayHit(getScreen, rays[i], 1.0,0);
-		if (info) {
-			getScreen.colors[i] = info.value().color;
-		}
+		getScreen.colors[i] = info;
 	}
 }
 
-std::optional<HitInfo> RayHit(Screen &getScreen, const Ray &getRay, double rayPower,int depth) {
+Color RayHit(Screen &getScreen, const Ray &getRay, double rayPower,int depth) {
 	//çƒãAíÜé~
 	if (rayPower < 0.01 || depth >= 36) {
-		return std::nullopt;
+		return Color(0,0,0);
 	}
 
 	for (int s = 0; s < getScreen.spheres.size(); s++) {
@@ -52,7 +50,7 @@ std::optional<HitInfo> RayHit(Screen &getScreen, const Ray &getRay, double rayPo
 		double D_4 = (std::pow(dotB, 2) - 4 * dotA*dotC) / 4;
 		double t1 = (-dotB / 2 + std::pow(D_4, 0.5)) / dotA;
 		double t2 = (-dotB / 2 - std::pow(D_4, 0.5)) / dotA;
-		auto vector_rayHit = std::vector<HitInfo>();
+		auto color = Color();
 		if (t1 >= DBL_EPSILON) {
 			for (double deg1 = 0; deg1 <= 2 * M_PI; deg1 += _DEGREE_DETAIL) {
 				for (double deg2 = -M_PI; deg2 <= M_PI ; deg2 += _DEGREE_DETAIL) {
@@ -76,15 +74,7 @@ std::optional<HitInfo> RayHit(Screen &getScreen, const Ray &getRay, double rayPo
 						hit.color = hit.dot* hit.hitObject.emission;
 						return hit;
 					}
-					//std::cout << V2string(Normalize(getRay.d + diffusionRay_direction)) << std::endl;
-					auto hitRecursive = RayHit(getScreen, Ray(hit.position, Normalize(getRay.d+diffusionRay_direction)), rayPower*hit.dot,depth+1);
-					if (hitRecursive) {
-						if (hitRecursive.value().hitObject.emission.Power() > 0) {
-							hit.color = hitRecursive.value().dot * hitRecursive.value().hitObject.emission;
-						}
-						else {
-							hit.color = hitRecursive.value().color * hit.dot;
-						}
+					
 					}
 					else {
 						//êFÇÃâeãøÇ™Ç≤Ç≠ÇÌÇ∏Ç©Ç≈Ç†ÇÈ
--- ray-tracing/render.h
+++ ray-tracing/render.h
@@ -3,6 +3,6 @@
 
 #include "def.h"
 
-std::optional<HitInfo> RayHit(Screen &, const Ray &,  double,int);
+Color RayHit(Screen &, const Ray &,  double,int);
 
 void render(Screen &);
